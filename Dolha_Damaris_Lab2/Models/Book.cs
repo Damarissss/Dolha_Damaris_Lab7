87e9146478a4feb56cc75995879020037f79c28f
Damarissss <damaris_dolha@yahoo.com>
2022-11-23 00:30:27 +0200

    Add project files.

---
+++ Dolha_Damaris_Lab2/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using MessagePack;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dolha_Damaris_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        // Foreign key
+        [Display(Name = "Author")]
+        public int AuthorID { get; set; }
+        // Navigation property. Author property holds a single Author entity (one-to-zero-or-one relationship)
+        public Author? Author { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+        public ICollection<PublishedBook>? PublishedBooks { get; set; }
+    }
+}
