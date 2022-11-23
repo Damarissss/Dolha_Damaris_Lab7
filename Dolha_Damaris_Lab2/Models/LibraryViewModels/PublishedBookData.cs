4718dae0f4f6137ad5cbbf6cced25946b0945092
Damarissss <damaris_dolha@yahoo.com>
2022-11-24 00:01:32 +0200

    master

---
+++ Dolha_Damaris_Lab2/Models/LibraryViewModels/PublishedBookData.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryModel.Models.LibraryViewModels
+{
+    public class PublishedBookData
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public bool IsPublished { get; set; }
+
+    }
+}
